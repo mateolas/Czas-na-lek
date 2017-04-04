d3392c66ef7500a19c85aec46ddeb813715cd879
mateolas <mateusz.kowara@gmail.com>
2017-04-04 21:44:43 +0200

    Added dialog_add layout - on this layout user will enter data regarding each medicine

--- app/src/main/java/com/studio/skyline/wezlek/ActivityMain.java
+++ app/src/main/java/com/studio/skyline/wezlek/ActivityMain.java
@@ -3,13 +3,24 @@ package com.studio.skyline.wezlek;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
     Toolbar mToolbar;
+    Button mBtnAdd;
+    private View.OnClickListener mBtnAddListener = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(ActivityMain.this, "Button was clicked", Toast.LENGTH_SHORT).show();
+        }
+    };
 
 
     @Override
@@ -17,6 +28,8 @@ public class ActivityMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.btn_dodaj_lek);
+        mBtnAdd.setOnClickListener(mBtnAddListener);
         setSupportActionBar(mToolbar);
         initBackgroundImage();
     }
