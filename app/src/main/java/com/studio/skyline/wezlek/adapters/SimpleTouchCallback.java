9d16f3c242fb03fd9df4ec5d5d84c3b004accf6b
mateolas <mateusz.kowara@gmail.com>
2017-05-01 11:28:43 +0200

    Disabling footer to swipe

--- app/src/main/java/com/studio/skyline/wezlek/adapters/SimpleTouchCallback.java
+++ app/src/main/java/com/studio/skyline/wezlek/adapters/SimpleTouchCallback.java
@@ -1,5 +1,6 @@
 package com.studio.skyline.wezlek.adapters;
 
+import android.graphics.Canvas;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
@@ -35,7 +36,23 @@ public class SimpleTouchCallback extends ItemTouchHelper.Callback {
     }
 
     @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        if(viewHolder instanceof AdapterDrops.DropHolder) {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    }
+
+    @Override
+    public void onChildDrawOver(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        if(viewHolder instanceof AdapterDrops.DropHolder) {
+            super.onChildDrawOver(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    }
+
+    @Override
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-        mListener.onSwipe(viewHolder.getAdapterPosition());
+        if(viewHolder instanceof AdapterDrops.DropHolder){
+            mListener.onSwipe(viewHolder.getAdapterPosition());
+        }
     }
 }
